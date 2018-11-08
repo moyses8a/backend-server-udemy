1bad6f3eec567a904b0da27b98324492fe33ba2d
moises8a <moyses8a@gmail.com>
2018-11-07 23:49:29 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+//  Inicializar variables
+var app = express();
+
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+    console.log("Base de datos: \x1b[32m%s\x1b[0m", "Online")
+});
+
+// Rutas
+app.get("/", (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    });
+});
+
+// Escuchar peticiones
+app.listen(3000, () => console.log("Express server puesto 3000: \x1b[32m%s\x1b[0m", "Online"));
